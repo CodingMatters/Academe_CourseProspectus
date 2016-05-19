76aba60fdc4276665f638e58fd4f6af0fcff98b2
gabbydgab <gamba@gabbydgab.com>
2016-05-19 15:33:02 +0800

    code sniffer fixes

--- config/autoload/router.config.php
+++ config/autoload/router.config.php
@@ -5,7 +5,7 @@ use CodingMatters\Kernel;
 
 return [
     "dependencies" =>  [
-        'aliases'       => [],        
+        'aliases'       => [],
         'invokables'    => [],
         'factories'     => [
             Prospectus\Page\IndexPage::class => Kernel\Factory\PageFactory::class
